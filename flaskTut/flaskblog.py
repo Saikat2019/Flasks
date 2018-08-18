6ec26978319cf656d40eca686da97e33a836a5fd
macoda <mad.sm.productions.com>
2018-08-18 18:25:32 +0530

    completed how to login and register , before using databases for storing users information

--- flaskTut/flaskblog.py
+++ flaskTut/flaskblog.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash,redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY']='83hgwdy2873hdw5723871g1y4817usg1'
 
 postLists = [
 	{
@@ -17,7 +20,7 @@ postLists = [
 ]
 
 @app.route("/")
-@app.route("/home")
+@app.route("/Home")
 def home():
     return render_template('home.html',posts=postLists)
 
@@ -25,5 +28,24 @@ def home():
 def about():
     return render_template('about.html',title='About')
 
+@app.route("/register",methods=['GET','POST'])    
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		flash('Account created!!', 'success')
+		return redirect(url_for('home'))
+	return render_template('register.html', title='Register', form=form)
+
+@app.route("/Login",methods=['GET','POST'])    
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		if form.email.data== 'admin@blog.com' and form.password.data== 'password':
+			flash('you have been logged in!','success')
+			return redirect(url_for('home'))
+		else:
+			flash('Login Unsuccessfull.Please check username and password','danger')	
+	return render_template('login.html',title='Login',form=form)		
+	
 if __name__ == '__main__':
 	app.run(debug=True)
